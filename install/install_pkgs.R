0c63e0159626cb8c348033c17494fd59dfe38170
dariober <dario.beraldi@glasgow.ac.uk>
2018-12-13 15:47:06 +0000

    R library created if none found

--- install/install_pkgs.R
+++ install/install_pkgs.R
@@ -22,24 +22,46 @@
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 # DEALINGS IN THE SOFTWARE.
 
-repos<- 'https://cran.r-project.org'
+# Download missing packages from this CRAN mirror
+REPOS<- 'https://cran.r-project.org'
 
-lib<- NULL
-for(x in .libPaths()){
-    if(file.access(x, mode= 2) == 0){
-        lib<- x
-        break
+# Install missing R packages in this directory. If NULL, attempt to find or
+# create a suitable one. If not NULL, this directory must exist and be on the
+# path of R libraries
+LIB<- NULL
+
+# -----------------------------------------------------------------------------
+
+if(is.null(LIB)){
+    # Get the first writable directory, if any
+    for(x in .libPaths()){
+        if(file.access(x, mode= 2) == 0){
+            LIB<- x
+            break
+        }
+    }
+    if(is.null(LIB)){
+        # Try to create a writable directory using the same strategy as
+        # install.packages
+        LIB <- unlist(strsplit(Sys.getenv("R_LIBS_USER"), .Platform$path.sep))[1L]
+        if (!file.exists(LIB)) {
+            if (!dir.create(LIB, recursive = TRUE)){
+                stop(gettextf("unable to create %s", sQuote(LIB)), domain = NA)
+            }
+            .libPaths(c(LIB, .libPaths()))
+        }
+    }
+    if(is.null(LIB)){
+        stop(sprintf('Cannot find a writable directory to install R packages.'))
     }
 }
 
-if(is.null(lib)){
-    stop(sprintf('Cannot find a writable directory to install R packages.'))
-}
+# -----------------------------------------------------------------------------
 
 tryCatch({
         suppressMessages(library(devtools))
     }, error = function(err) {
-        install.packages('devtools', repos= repos, lib= lib, Ncpus= 6)
+        install.packages('devtools', repos= REPOS, lib= LIB, Ncpus= 6)
     }
 )
 
@@ -47,55 +69,55 @@ tryCatch({
         suppressMessages(library(Rsamtools))
     }, error = function(err) {
         source("https://bioconductor.org/biocLite.R")
-        biocLite("Rsamtools", ask= FALSE, suppressUpdates= TRUE, lib= lib, Ncpus= 6)
+        biocLite("Rsamtools", ask= FALSE, suppressUpdates= TRUE, lib= LIB, Ncpus= 6)
     }
 )
 
 tryCatch({
         suppressMessages(library(data.table))
     }, error = function(err) {
-        install.packages('data.table', repos= repos, lib, Ncpus= 6)
+        install.packages('data.table', repos= REPOS, lib= LIB, Ncpus= 6)
     }
 )
 
 tryCatch({
         suppressMessages(library(ggplot2))
     }, error = function(err) {
-        install.packages('ggplot2', repos= repos, lib, Ncpus= 6)
+        install.packages('ggplot2', repos= REPOS, lib= LIB, Ncpus= 6)
     }
 )
 
 tryCatch({
         suppressMessages(library(gridExtra))
     }, error = function(err) {
-        install.packages('gridExtra', repos= repos, lib, Ncpus= 6)
+        install.packages('gridExtra', repos= REPOS, lib= LIB, Ncpus= 6)
     }
 )
 
 tryCatch({
         suppressMessages(library(argparse))
     }, error = function(err) {
-        devtools::install_github("trevorld/argparse", ref= "v1.1.1", repos= repos, lib= lib)
+        devtools::install_github("trevorld/argparse", ref= "v1.1.1", repos= REPOS, lib= LIB)
     }
 )
 
 tryCatch({
         suppressMessages(library(facets))
     }, error = function(err) {
-        devtools::install_github("mskcc/facets", ref = "434b5ce", lib= lib)
+        devtools::install_github("mskcc/facets", ref = "434b5ce", lib= LIB)
     }
 )
 
 tryCatch({
         suppressMessages(library(testthat))
     }, error = function(err) {
-        install.packages('testthat', repos= repos, lib, Ncpus= 6)
+        install.packages('testthat', repos= REPOS, lib= LIB, Ncpus= 6)
     }
 )
 
 tryCatch({
         suppressMessages(library(covr))
     }, error = function(err) {
-        install.packages('covr', repos= repos, lib, Ncpus= 6)
+        install.packages('covr', repos= REPOS, lib= LIB, Ncpus= 6)
     }
 )
